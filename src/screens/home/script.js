f81f90c163730ee944393e57e1c1814f2e2ec35e
Rosivan Cardoso Ferreira <rosivancardoso767@gmail.com>
2022-09-30 23:55:09 -0400

    feat: Header responsivo

--- src/screens/home/script.js
+++ src/screens/home/script.js
@@ -1,7 +1,27 @@
 const fields = {
-    header: document.getElementById("header")
+    header: document.getElementById("header"),
+    headerNavigation: document.getElementById("header-navigation"),
+    headerMenu: document.getElementById("header-menu"),
+    headerItems: document.getElementById("header-items"),
 }
  window.addEventListener("scroll", () => {
     const currentPositionScroll = window.pageYOffset;
     fields.header.classList.toggle("header--show", currentPositionScroll > 150)
  })
+
+ function onOpenMenu() {
+    const isCloseMenu = !fields.headerNavigation.classList.contains("header__navigation--open");
+    fields.headerNavigation.classList.toggle("header__navigation--open", isCloseMenu);
+    fields.headerMenu.classList.toggle("header__menu--active", isCloseMenu);
+    fields.headerItems.classList.toggle("header__items--open", isCloseMenu);
+ }
+
+ function onCloseMenu() {
+    const isOpenMenu = fields.headerNavigation.classList.contains("header__navigation--open");
+    if(isOpenMenu) {
+        fields.headerNavigation.classList.remove("header__navigation--open");
+        fields.headerNavigation.classList.remove("header__navigation--open");
+        fields.headerMenu.classList.remove("header__menu--active");
+        fields.headerItems.classList.remove("header__items--open");
+    }
+ }
